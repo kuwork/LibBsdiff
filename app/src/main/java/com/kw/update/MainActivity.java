8e7cd6bcb33bff5980a8fb5588d6566fb034a89f
Jerry <jerry535362936@126.com>
2017-06-13 11:30:06 +0800

    创建项目

---
+++ app/src/main/java/com/kw/update/MainActivity.java
@@ -0,0 +1,22 @@
+package com.kw.update;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void click(View view) {
+
+    }
+
+
+}
