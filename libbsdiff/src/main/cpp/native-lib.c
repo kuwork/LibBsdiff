a00e24bc81f81ab2df1d661b2c2a5f2614ab13cd
Jerry <jerry535362936@126.com>
2017-06-13 13:15:35 +0800

    CMakeLists 更新，使用更简便的方式

--- libbsdiff/src/main/cpp/native-lib.c
+++ libbsdiff/src/main/cpp/native-lib.c
@@ -3,16 +3,15 @@
 JNIEXPORT jint JNICALL
 Java_com_kw_lib_bsdiff_BsdiffUtils_diff(JNIEnv *env, jclass type, jstring oldpath_,
                                         jstring newpath_, jstring patch_) {
-    const char *argv[4];
+    char *argv[4];
     argv[0] = "bsdiff";
     argv[1] = (*env)->GetStringUTFChars(env, oldpath_, 0);
     argv[2] = (*env)->GetStringUTFChars(env, newpath_, 0);
     argv[3] = (*env)->GetStringUTFChars(env, patch_, 0);
-    bsdiff_main(4, argv);
+    bsdiff_main(4, (char *)argv);
     (*env)->ReleaseStringUTFChars(env, oldpath_, argv[1]);
     (*env)->ReleaseStringUTFChars(env, newpath_, argv[2]);
     (*env)->ReleaseStringUTFChars(env, patch_, argv[3]);
-    free(argv[0]);
     free(argv);
     return 0;
 }
@@ -20,12 +19,12 @@ Java_com_kw_lib_bsdiff_BsdiffUtils_diff(JNIEnv *env, jclass type, jstring oldpat
 JNIEXPORT jint JNICALL
 Java_com_kw_lib_bsdiff_BsdiffUtils_patch(JNIEnv *env, jclass type, jstring oldpath_,
                                          jstring newpath_, jstring patch_) {
-    const char *argv[4];
+    char *argv[4];
     argv[0] = "bspatch";
     argv[1] = (*env)->GetStringUTFChars(env, oldpath_, 0);
     argv[2] = (*env)->GetStringUTFChars(env, newpath_, 0);
     argv[3] = (*env)->GetStringUTFChars(env, patch_, 0);
-    bspatch_main(4, argv);
+    bspatch_main(4, (char *)argv);
     (*env)->ReleaseStringUTFChars(env, oldpath_, argv[1]);
     (*env)->ReleaseStringUTFChars(env, newpath_, argv[2]);
     (*env)->ReleaseStringUTFChars(env, patch_, argv[3]);
